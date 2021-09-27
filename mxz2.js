7317ae5504e3953ee99dc9e92ed13a9aa59d1150
jifengdong <58116847+jifengdong@users.noreply.github.com>
2021-09-27 11:38:27 +0800

    Create mxz2.js

---
+++ mxz2.js
@@ -0,0 +1,17 @@
+/*
+密小助解锁VIP，恋爱话语
+https://apic.yzhiyin.com/index.php/v4/new/search/search
+*/
+
+let obj = JSON.parse($response.body);
+
+obj.data = {
+     "category_id": 2,
+      "sex_type": 1,
+      "status": 1,
+      "quiz_sex": 2,
+      "ans_sex": 1,
+      "ans_type": 1,
+      "recommended": 0
+}
+$done({body: JSON.stringify(obj)});
